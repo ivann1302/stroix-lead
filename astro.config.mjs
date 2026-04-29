714b50bf011b3ea7ac54ff0511493d4ae3a7d62d
ivan <ivann97n@gmail.com>
2026-04-29 17:27:57 +0300

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: process.env.SITE_URL || 'https://stroix-leads.ru',
+  base: process.env.BASE_PATH || '/',
   output: 'static',
   integrations: [react(), sitemap()],
 });
